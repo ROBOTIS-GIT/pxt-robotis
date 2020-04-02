fab6174a4002abeaeaa363c7584063848de90e10
sweetfood-dev_company <kjsii123@hanmail.net>
2020-04-02 15:54:33 +0900

    block move robotiscategory1.ts -> screen---st7735/targetoverrides.ts

--- libs/robotis-cm-300/robotiscategory1.ts
+++ libs/robotis-cm-300/robotiscategory1.ts
@@ -6,12 +6,12 @@ namespace robotiscategory1 {
     export function testBlock(){
 
     }    
-
+ /*
     //% group="Screen"
     //% weight=24
     //% blockId=dxl_image_background block="set background image to %img=background_image_picker"
     export function robotisBackgroundImage(img: Image) {
         
-    }
+    }*/
 
 }
--- libs/screen---st7735/targetoverrides.ts
+++ libs/screen---st7735/targetoverrides.ts
@@ -46,3 +46,19 @@ namespace _screen_internal {
         return img as ScreenImage;
     }
 }
+
+/**
+ * Communication between ROBOTIS's DYNAMIXEL devices
+ */
+//% block="robotiscategory1"
+//% color=#4b6584 weight=89 icon="\uf1bb"
+//% groups='["Screen", "Effects", "Tiles"]'
+namespace robotiscategory1 {
+    
+    //% group="Screen"
+    //% weight=24
+    //% blockId=dxl_image_background block="set background image to %img=background_image_picker"
+    export function robotisBackgroundImage(img: Image) {
+        
+    }
+}
